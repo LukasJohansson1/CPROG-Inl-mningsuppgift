2531a8fef8c22b840912d7352857a641ce496de0
Lukas Johansson <lukasjohanssonjp@gmail.com>
2026-01-07 19:17:21 +0100

    Change background

--- include/Constants.h
+++ include/Constants.h
@@ -6,13 +6,14 @@
 namespace constants
 {
     // Constants
-   	constexpr int gScreenWidth     { 800 };
-   	constexpr int gScreenHeight    { 480 };
+   	constexpr int gScreenWidth     { 1280 };
+   	constexpr int gScreenHeight    { 720 };
    	// PATH TO YOUR RESOURCE FOLDER 'resources', 'gResPath'
    	const std::string gResPath { "./resources/" };
 	// PATH'S TO ALL YOUR EXTERNAL RESOURCES using 'gResPath'
-	const std::string bg_str  { gResPath + "images/bg.jpg" };
+	const std::string bg_str  { gResPath + "images/scary-background.png" };
     const std::string sample_str { gResPath + "sounds/sample.wav"};
+	const std::string player_charart {gResPath + "images/Walk.png"};
 }
 
 #endif
